ac3a9f0eb861658b260c3522edc8bed76bfda64b
velissariou <it12143@uom.edu.gr>
2020-04-09 14:52:23 +0300

    Small linked lists programs

---
+++ a2af4.c
@@ -0,0 +1,150 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NumberOfNodes 5
+#define NilValue -1
+
+typedef int ListElementType;
+
+typedef int ListPointer;
+
+typedef struct {
+    ListElementType Data;
+    ListPointer  Next;
+} NodeType;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+void InitializeStoragePool(NodeType Node[], ListPointer *FreePtr);
+void CreateLList(ListPointer *List);
+boolean EmptyLList(ListPointer List);
+boolean FullLList(ListPointer FreePtr);
+void GetNode(ListPointer *P, ListPointer *FreePtr, NodeType Node[]);
+void ReleaseNode(NodeType Node[NumberOfNodes], ListPointer P, ListPointer *FreePtr);
+void Insert(ListPointer *List, NodeType Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item);
+void Delete(ListPointer *List, NodeType Node[], ListPointer *FreePtr, ListPointer PredPtr);
+void TraverseLinked(ListPointer List, NodeType Node[]);
+
+
+int main(){
+
+}
+
+
+#include <stdio.h>
+#include "L_ListADT.h"
+
+void InitializeStoragePool(NodeType Node[], ListPointer *FreePtr)
+
+{
+   int i;
+
+    for (i=0; i<NumberOfNodes-1;i++)
+    {
+        Node[i].Next=i+1;
+        Node[i].Data=-1;
+    }
+    Node[NumberOfNodes-1].Next=NilValue;
+    Node[NumberOfNodes-1].Data=NilValue;
+    *FreePtr=0;
+}
+
+void CreateLList(ListPointer *List)
+
+{
+  *List=NilValue;
+}
+
+boolean EmptyLList(ListPointer List)
+
+{
+  return (List==NilValue);
+}
+
+boolean FullLList(ListPointer FreePtr)
+
+{
+  return (FreePtr == NilValue);
+}
+
+void GetNode(ListPointer *P, ListPointer *FreePtr, NodeType Node[])
+
+{
+  *P = *FreePtr;
+  if (!FullLList(*FreePtr))
+    *FreePtr =Node[*FreePtr].Next;
+}
+
+void ReleaseNode(NodeType Node[], ListPointer P, ListPointer *FreePtr)
+
+{
+  Node[P].Next =*FreePtr;
+  Node[P].Data = -1;
+
+  *FreePtr =P;
+}
+
+void Insert(ListPointer *List, NodeType Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item)
+
+{
+  ListPointer TempPtr;
+  GetNode(&TempPtr,FreePtr,Node);
+  if (!FullLList(TempPtr)) {
+    if (PredPtr==NilValue)
+    {
+        Node[TempPtr].Data =Item;
+        Node[TempPtr].Next =*List;
+        *List =TempPtr;
+    }
+    else
+      {
+        Node[TempPtr].Data =Item;
+        Node[TempPtr].Next =Node[PredPtr].Next;
+        Node[PredPtr].Next =TempPtr;
+      }
+}
+  else
+    printf("Full List ...\n");
+}
+
+void Delete(ListPointer *List, NodeType Node[], ListPointer *FreePtr, ListPointer PredPtr)
+
+{
+  ListPointer TempPtr ;
+
+  if (!EmptyLList(*List))
+    if (PredPtr == NilValue)
+    {
+        TempPtr =*List;
+        *List =Node[TempPtr].Next;
+        ReleaseNode(Node,TempPtr,FreePtr);
+    }
+    else
+      {
+        TempPtr =Node[PredPtr].Next;
+        Node[PredPtr].Next =Node[TempPtr].Next;
+        ReleaseNode(Node,TempPtr,FreePtr);
+      }
+  else
+    printf("Empty List ...\n");
+}
+
+void TraverseLinked(ListPointer List, NodeType Node[])
+
+{
+  ListPointer CurrPtr;
+
+  if (!EmptyLList(List))
+  {
+      CurrPtr =List;
+      while (CurrPtr != NilValue)
+      {
+          printf("(%d: %d,%d) ",CurrPtr,Node[CurrPtr].Data, Node[CurrPtr].Next);
+          CurrPtr=Node[CurrPtr].Next;
+       }
+       printf("\n");
+   }
+  else printf("Empty List ...\n");
+}
